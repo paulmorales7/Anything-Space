dd43aeb442cb553a7ce1ce3675dd3a0951d15dfe
Morales <pmorales@activision.com>
2022-04-04 17:34:05 -0500

    added desc for leo, cancer, and virgo

--- script.js
+++ script.js
@@ -113,3 +113,111 @@ function geminiFunc() {
 
     document.querySelector('#signTitle').innerHTML = 'Gemini'
 }
+
+function cancerFunc() {
+
+     let image = document.querySelector('#starImg');
+     image.src = "./images/cancer-star.png"
+
+     var styles = {
+         "margin-bottom": "1.2rem",
+         "width": "195px"
+     }
+
+    Object.assign(image.style, styles)
+    
+    document.querySelector('#mainPara').innerHTML = 'Cancers have a reputation for being hyper emotional, temperamental, and spiteful. Cancers, in additional to being devoted, are extremely fond of their loved ones, often to an unhealthy degree. They place a high value on family and close friends, and will go to great lengths to defend them, no matter the price. Very reliant on their instincts, usually acting upon them.'
+
+    document.querySelector('#descSign').innerHTML = 'Element: WATER'
+
+    document.querySelector('#descDate').innerHTML = 'Dates: JUNE 21 - JULY 22'
+
+    document.querySelector('#descComp').innerHTML = 'Compatibility: CAPRICORN, TAURUS'
+
+    document.querySelector('#descColor').innerHTML = 'Color: WHITE'
+
+    document.querySelector('#descNum').innerHTML = 'Lucky Numbers: 2, 3, 15, 20'
+
+    document.querySelector('#signTitle').innerHTML = 'Cancer'
+}
+
+function cancerFunc() {
+
+     let image = document.querySelector('#starImg');
+     image.src = "./images/cancer-star.png"
+
+     var styles = {
+         "margin-bottom": "1.2rem",
+         "width": "195px"
+     }
+
+    Object.assign(image.style, styles)
+    
+    document.querySelector('#mainPara').innerHTML = 'Cancers have a reputation for being hyper emotional, temperamental, and spiteful. Cancers, in additional to being devoted, are extremely fond of their loved ones, often to an unhealthy degree. They place a high value on family and close friends, and will go to great lengths to defend them, no matter the price. Very reliant on their instincts, usually acting upon them.'
+
+    document.querySelector('#descSign').innerHTML = 'Element: WATER'
+
+    document.querySelector('#descDate').innerHTML = 'Dates: JUNE 21 - JULY 22'
+
+    document.querySelector('#descComp').innerHTML = 'Compatibility: CAPRICORN, TAURUS'
+
+    document.querySelector('#descColor').innerHTML = 'Color: WHITE'
+
+    document.querySelector('#descNum').innerHTML = 'Lucky Numbers: 2, 3, 15, 20'
+
+    document.querySelector('#signTitle').innerHTML = 'Cancer'
+}
+
+function leoFunc() {
+
+     let image = document.querySelector('#starImg');
+     image.src = "./images/leo-star.png"
+
+     var styles = {
+         "margin-bottom": "1.2rem",
+         "width": "195px"
+     }
+
+    Object.assign(image.style, styles)
+    
+    document.querySelector('#mainPara').innerHTML = 'Leos are the natural leaders of the zodiac, as magnificent and striking as the Lion that embodies their sign. Leos are radiantly joyful, liberal with their appeal and endowments. They are fiercely proud and confident. They love and live life to the fullest rather than being in charge at home, work, and play.Leos are very loving and creative, this is why there are many famous Leo performers in the world.'
+
+    document.querySelector('#descSign').innerHTML = 'Element: FIRE'
+
+    document.querySelector('#descDate').innerHTML = 'Dates: JULY 23 - AUGUST 22'
+
+    document.querySelector('#descComp').innerHTML = 'Compatibility: AQUARIUS, GEMINI'
+
+    document.querySelector('#descColor').innerHTML = 'Color: GOLD, YELLOW, ORANGE'
+
+    document.querySelector('#descNum').innerHTML = 'Lucky Numbers: 1, 3, 10, 19'
+
+    document.querySelector('#signTitle').innerHTML = 'Leo'
+}
+
+function virgoFunc() {
+
+     let image = document.querySelector('#starImg');
+     image.src = "./images/virgo-star.png"
+
+     var styles = {
+         "margin-bottom": "1.2rem",
+         "width": "195px"
+     }
+
+    Object.assign(image.style, styles)
+    
+    document.querySelector('#mainPara').innerHTML = 'Virgos are known for being perfectionists, and can be meticulous and single-minded in their pursuit of improvement. However, this also makes them extremely dedicated to the people in their lives, and helps them find success professionally. Virgos are very hard workers and are not afraid to throw themselves into work, no matter the difficulty of the task. This also means they have high standards and can be overly judgemental and critical towards others and their work.'
+
+    document.querySelector('#descSign').innerHTML = 'Element: EARTH'
+
+    document.querySelector('#descDate').innerHTML = 'Dates: AUGUST 23 - SEPTEMBER 22'
+
+    document.querySelector('#descComp').innerHTML = 'Compatibility: PISCES, CANCER'
+
+    document.querySelector('#descColor').innerHTML = 'Color: GREY, BEIGE, PALE-YELLOW'
+
+    document.querySelector('#descNum').innerHTML = 'Lucky Numbers: 5, 14, 15, 23, 32'
+
+    document.querySelector('#signTitle').innerHTML = 'Virgo'
+}
